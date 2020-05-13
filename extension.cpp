34583f0a16b67b2ef6335eb3c075c85ae691bebc
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-05-13 08:03:53 -0700

    simplify

--- extension.cpp
+++ extension.cpp
@@ -2,21 +2,12 @@
 using namespace pxt;
 
 namespace dataStreamer {
-   //%
-   void init() {
-     uBit.serial.redirect(USBTX, USBRX);
-     uBit.serial.baud(9600);
-   }
-
-    /**
-    * Set the USB baud rate connection.
-    * @param rate the new baud rate.
-    */
-    //% weight=10
-    //% blockId=datastreamer_setBaud block="set baud rate %rate"
-    //% rate.defl=9600
-
+    //%
    void setBaudRate(int rate) {
-    uBit.serial.baud(rate);
+#if MICROBIT_CODAL     
+     uBit.serial.setBaud(9600);
+#elif
+     uBit.serial.baud(9600);
+#endif     
    }
 }
